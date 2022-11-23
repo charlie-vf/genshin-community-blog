d1ec3c6cd7e2207303a598b1c3d103860d9dacfd
Charlie <charlottevfarmer@gmail.com>
2022-11-23 15:23:55 +0000

    Import static files for BootStrap template

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import BlogPost, Category
+from .models import BlogPost, Category, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(BlogPost)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
+    prepopulated_fields = {('slug'): ('title',)}
+    list_filter = ('status', 'created_date')
+    list_display = ('title', 'slug', 'status', 'created_date')
+    search_fields = ['title', 'content']
+
+
+@admin.register(Comments)
+class PostComment(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_date', 'approved')
+    list_filter = ('approved', 'created_date')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 
 
 admin.site.register(Category)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views import generic, View
 
 # Create your views here.
+
