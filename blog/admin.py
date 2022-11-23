372823f4c05e8bb6bd56b7524d7c08389e4f2cad
Charlie <charlottevfarmer@gmail.com>
2022-11-23 14:27:36 +0000

    Get PostAdmin setup & begin adding categories filter

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    category = models.CharField(max_length=200, default='general')
+    featured_image = CloudinaryField('image', default='default')
+    exerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='blog_comments')
+    name = models.CharField(max_length=90)
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    comments = models.ManyToManyField(User, related_name='post_comments', blank=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+    def number_of_comments(self):
+        return self.comments.count()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
