984bb3c9308e83b89c2d8c361064b946d8b8442a
Charlie <charlottevfarmer@gmail.com>
2022-11-23 15:39:39 +0000

    Add base.html & reformat from imported index.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views import generic, View
+from .models import BlogPost
 
-# Create your views here.
 
+class BlogPostList(generic.ListView):
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_date')
+    template_name = 'index.html'
+    paginate_by = 6
